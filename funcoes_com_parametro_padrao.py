05d33bad312a10d47b3f415a84e6c3ca555f68d5
Fabio Landin <flandin1990@gmail.com>
2025-05-13 02:06:21 -0300

    Finalizando aula de função com parametros padrões

---
+++ funcoes_com_parametro_padrao.py
@@ -0,0 +1,39 @@
+"""
+Funções com parametro padrão (Default Paramters)
+
+-Funções onde a passagem de parametros seja opcional.
+
+
+"""
+from Ex_modulo4 import contador
+
+
+#Exemplo 1 - se colocarmos o valor na função, ele fica como padrão, mas pode ser alterado se repassarmos ele
+
+def exponencial(numero=2, potencia=2):
+    return numero ** potencia
+
+print(exponencial(2, 3))
+print(exponencial())
+
+#Exemplo mais complexo
+def mostra_informacao(nome='Geek', instrutor=False):
+    if nome == 'Geek' and instrutor:
+        return 'Bem vindo instrutor Fabio'
+    elif nome == 'Geek':
+        return 'Eu pensei que voce era o instrutor Fabio'
+    return f'Olá {nome}'
+
+print(mostra_informacao())
+print(mostra_informacao(instrutor=True))
+print(mostra_informacao('Gabriel', instrutor=True))
+
+# Podemos ter funções que são declaradas dentro de funções
+
+def fora():
+    contador = 0
+    def dentro():
+        nonlocal contador
+        contador = contador + 1
+        return contador
+    return dentro()
