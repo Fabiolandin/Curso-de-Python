3800e5eee98b13113f7cface3c57117a3a25f792
Fabio Landin <flandin1990@gmail.com>
2025-05-14 14:23:50 -0300

    Finalizando aula sobre List Comprehensio parte 1

---
+++ list_comprehension_1.py
@@ -0,0 +1,46 @@
+"""
+List Comprehension
+
+- Utilizando list comprehension nos podemos gerar novas listas com daods processados a partir de outro iteravel.
+
+# Sintaxe da list comprehension
+[ dado  for dado in iteravel ]
+"""
+
+#Exemplos
+numeros = [1, 2, 3, 4, 5]
+
+res = [numero * 10 for numero in numeros]
+print(res)
+
+"""
+Para entender melhor oque esta acontecendo devemos dividir a expressão em duas pertes
+1 - for numero in numeros
+2 - numero * 10
+"""
+
+res = [numero / 2 for numero in numeros]
+print(res)
+
+#List comprehension vs Loop
+
+#Loop
+numeross = [1, 2, 3, 4, 5]
+numeros_dobrados = []
+
+for numero in numeross:
+    numero_dobrado = numero * 2
+    numeros_dobrados.append(numero_dobrado)
+
+print(numeros_dobrados)
+
+#List comprehension
+print([numero * 2 for numero in numeros])
+
+# Exemplo 3
+def caixa_alta(nome):
+    nome = nome.replace(nome[0], nome[0].upper())
+    return nome
+
+amigos = ['fabio', 'guilherme', 'carlos', 'marcos', 'fabiano']
+print([caixa_alta(amigo) for amigo in amigos])
