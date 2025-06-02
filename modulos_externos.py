a4398ddb66efd1848dd6e947242384a0b01a102b
Fabio Landin <flandin1990@gmail.com>
2025-06-02 11:58:38 -0300

    Finalizando aula sobre módulos externos

---
+++ modulos_externos.py
@@ -0,0 +1,14 @@
+"""
+Módulos Externos
+
+Utilizamos o gerenciador de pacotes Python chamado Pip - Python Installer Package
+
+https://pypi.org
+
+colorama -> é utilizado para permitir impressão de textos coloridos no terminal
+"""
+from colorama import init, Fore
+init()
+
+print(Fore.RED + 'Fabio Landin')
+print(Fore.BLUE + 'Fabio Landin')
