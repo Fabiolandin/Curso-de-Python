50c3f2fad30f4e2e804aa7fcc6f8f9a189b768e8
Fabio Landin <flandin1990@gmail.com>
2025-06-02 17:51:48 -0300

    Finalizando aula sobre dunder

---
+++ dunder.py
@@ -0,0 +1,11 @@
+"""
+Dunder Name e Dunder Main
+
+Dunder -> Double Under __
+
+Dunder Name -> __name__
+Dunder Main -> __main__
+
+Em python são utilizados dunder para criar funções/atributos/e etc utilizando Double under para não gerar
+conflito com os nomes dos elementos.
+"""
