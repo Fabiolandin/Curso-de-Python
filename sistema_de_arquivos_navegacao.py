96cbea91389c7399f58d42b8de918783f8b45d2b
Fabio Landin <flandin1990@gmail.com>
2025-06-04 14:09:40 -0300

    Finalizando aula sobre sistema de arquivos navegacao

---
+++ sistema_de_arquivos_navegacao.py
@@ -0,0 +1,37 @@
+"""
+Sistema de arquivos e navegação
+
+Para fazer uso de manipulação de arquivos do sistema operacional, precisamos importar e fazer uso do módulo os.
+
+os -> Operating System - Sistema operacional
+"""
+#Fazer o import
+import os
+
+#getcwd() -> pega o current work directory - diretório de trabalho recorrente
+#Retorna o path (caminho) absoluto
+print(os.getcwd())
+
+#Para mudar o diretório podemos utilizar o chdir()
+os.chdir('..')
+
+print(os.getcwd())
+
+#Usuarios Windows
+#Ter cuidado ao verificar diretórios
+print(os.path.isabs('C:\\Users\\Fabio\\PycharmProjects\\Curso-de-Python'))
+
+#Podemos identificar o sistema operacional com o módulo os NT = windows, posix = mac, linux
+print(os.name)
+
+# Podemos listar os arquivos e diretórios com mais detalhes com scandir()
+scanner = os.scandir()
+arquivos = list(os.scandir())
+print(arquivos)
+
+print(arquivos[0].name) #Nome do arquivo
+print(arquivos[0].path) #Caminho até o arquivo
+print(arquivos[0].stat()) # Estatisticas sobre o arquivo
+print(arquivos[0].is_dir()) #É um diretório? True
+
+scanner.close()
